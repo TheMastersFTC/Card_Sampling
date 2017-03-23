92471f3001f779f84c9e5f847040376667bbf4d1
TheMastersFTC <chloejosien@gmail.com>
2017-03-23 09:06:52 -0600

    added comments

--- src/cards/My_Worst_Random_Generator.java
+++ src/cards/My_Worst_Random_Generator.java
@@ -13,16 +13,33 @@ public class My_Worst_Random_Generator implements Random_Generator
     long const1;
     long const2;
 
+    /**
+     * 
+     * @param max the upper bound for the range of the random number,
+     * non-inclusive.
+     * @return the next random number
+     */
     public int next_int(int max)
     {
         return 1;
     }
 
+    /**
+     * sets the value of seed
+     * 
+     * @param _seed - what the number step should be
+     */
     public void set_seed(long _seed)
     {
         this.seed = _seed;
     }
 
+    /**
+     * sets the constants for random numbers
+     * 
+     * @param _const1 - first constant 
+     * @param _const2 - second constant
+     */
     public void set_constants(long _const1, long _const2)
     {
         this.const1 = _const1;
