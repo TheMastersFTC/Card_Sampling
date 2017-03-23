12e06b2abcf4a80b694b0007fd0c3208bba6f8a8
TheMastersFTC <chloejosien@gmail.com>
2017-03-23 09:34:16 -0600

    added comments will need more when done

--- src/cards/Main.java
+++ src/cards/Main.java
@@ -1,10 +1,25 @@
 package cards;
 
 /**
- * Created by chloe on 3/19/2017.
+ * Created by chloe and kylee fluckiger.
+ * finds all the values from the odds class
  */
 public class Main {
+    /**
+     * runs all the methods in odds.
+     * @param args
+     */
     public static void main(String args[]){
+
+//        double[] fiveCardHistogram = Odds.percentage_per_hand_category_exhaustive(5);
+//        for(int count=0; count<fiveCardHistogram.length; count++){
+//            System.out.println(fiveCardHistogram[count]*100);
+//        }
+
+        double[] sevenCardHistogram = Odds.percentage_per_hand_category_exhaustive(7);
+        for(int count=0; count<sevenCardHistogram.length; count++){
+            System.out.println(sevenCardHistogram[count]*100);
+        }
 
     }
 }
