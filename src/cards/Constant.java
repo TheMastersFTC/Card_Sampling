c1a2112c41dd9062758afc68dc0120d9f8a2cf26
TheMastersFTC <chloejosien@gmail.com>
2017-03-22 20:30:37 -0600

    added rankValues

--- src/cards/Constant.java
+++ src/cards/Constant.java
@@ -30,10 +30,22 @@ public class Constant {
     }
 
     public enum Ranks {
-        ROYAL_FLUSH, STRAIGHT_FLUSH, FOUR_OF_A_KIND,
-        FULL_HOUSE, FLUSH, STRAIGHT, THREE_OF_A_KIND,
-        TWO_PAIRS, SINGLE_PAIR, HIGH_CARD_ONLY;
-    }
+        //        ROYAL_FLUSH, STRAIGHT_FLUSH, FOUR_OF_A_KIND,
+//        FULL_HOUSE, FLUSH, STRAIGHT, THREE_OF_A_KIND,
+//         TWO_PAIRS, SINGLE_PAIR, HIGH_CARD_ONLY;
+        ROYAL_FLUSH(9), STRAIGHT_FLUSH(8), FOUR_OF_A_KIND(7),
+        FULL_HOUSE(6), FLUSH(5), STRAIGHT(4), THREE_OF_A_KIND(3),
+        TWO_PAIRS(2), SINGLE_PAIR(1), HIGH_CARD_ONLY(0);
 
+        int rank;
+
+        private Ranks(int rank) {
+            this.rank = rank;
+        }
+
+        public int getRankValue() {
+            return rank;
+        }
+    }
 }
 
