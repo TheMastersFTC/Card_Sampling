65c6ec2077f870990a6b12b405d0a795991e818a
ky-grr <kyl.flu@gmail.com>
2017-03-22 23:59:02 -0600

    random sampling function in Odds

--- src/cards/Odds.java
+++ src/cards/Odds.java
@@ -11,70 +11,92 @@ public class Odds {
     }
 
     public static double[] percentage_per_hand_category_exhaustive(int hand_size){
+    	
+    	double [] histogram = new double[10];
+    	
+    	if(hand_size == 5) {
+	    	for(int first=0; first<48; first++) {
+	    		for(int second=first+1; second<52; second++) {
+	    			for(int third=second+1; third<52; third++) {
+	    				for(int fourth=third+1; fourth<52; fourth++) {
+	    					for(int fifth=fourth+1; fifth<52; fifth++) {
+	    						
+	    						Hand hand = new Hand(Deck.deck[first], Deck.deck[second], Deck.deck[third], Deck.deck[fourth], Deck.deck[fifth]);
+	    						
+	    						System.out.println(hand.cards);
+	    						System.out.println(hand.rank);
+	    						System.out.println(hand.longestStraight());
+	    						System.out.println(hand.rank.ordinal());
 
-        double [] histogram = new double[10];
-
-        if(hand_size == 5) {
-            for(int first=0; first<48; first++) {
-                for(int second=first+1; second<52; second++) {
-                    for(int third=second+1; third<52; third++) {
-                        for(int fourth=third+1; fourth<52; fourth++) {
-                            for(int fifth=fourth+1; fifth<52; fifth++) {
-
-                                Hand hand = new Hand(Deck.deck[first], Deck.deck[second], Deck.deck[third], Deck.deck[fourth], Deck.deck[fifth]);
-                                histogram[hand.rank.ordinal()]++;
-
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if(hand_size == 7) {
-            for(int first=0; first<48; first++) {
-                for(int second=first+1; second<52; second++) {
-                    for(int third=second+1; third<52; third++) {
-                        for(int fourth=third+1; fourth<52; fourth++) {
-                            for(int fifth=fourth+1; fifth<52; fifth++) {
-                                for(int sixth = fifth+1; sixth<52; sixth++) {
-                                    for(int seventh = sixth+1; seventh<52; seventh++) {
-
-                                        Hand hand = new Hand(Deck.deck[first], Deck.deck[second], Deck.deck[third], Deck.deck[fourth], Deck.deck[fifth]);
-                                        histogram[hand.rank.ordinal()]++;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-
-        int totalHands = 0;
-
-        //Calculate the total number of hands.
-        for(int index=0; index<histogram.length; index++) {
-            totalHands += histogram[index];
-        }
-
-        for(int index=0; index<histogram.length; index++) {
-            histogram[index] = histogram[index]/totalHands;
-        }
+	    						histogram[hand.rank.ordinal()]++;
+	    						
+	    					}
+	    				}
+	    			}
+	    		}
+	    	}
+    	}
+    	    	
+    	if(hand_size == 7) {
+	    	for(int first=0; first<46; first++) {
+	    		for(int second=first+1; second<52; second++) {
+	    			for(int third=second+1; third<52; third++) {
+	    				for(int fourth=third+1; fourth<52; fourth++) {
+	    					for(int fifth=fourth+1; fifth<52; fifth++) {
+	    						for(int sixth = fifth+1; sixth<52; sixth++) {
+	    							for(int seventh = sixth+1; seventh<52; seventh++) {
+	    								
+	    	    						Hand hand = new Hand(Deck.deck[first], Deck.deck[second], Deck.deck[third], Deck.deck[fourth], Deck.deck[fifth], Deck.deck[sixth], Deck.deck[seventh]);
+	    	    						histogram[hand.rank.ordinal()]++;	    	    						
+	    	    						
+	    							}
+	    						}
+	    					}
+	    				}
+	    			}
+	    		}
+	    	}
+    	
+    		
+    	}
+    	
+    	
+    	int totalHands = 0;
+    	
+    	//Calculate the total number of hands.
+    	for(int index=0; index<histogram.length; index++) {
+    		totalHands += histogram[index];
+    	}
+    	
+    	for(int index=0; index<histogram.length; index++) {
+    		histogram[index] = histogram[index]/totalHands;
+    		histogram[index] = histogram[index] * 100;
+    	}
 
         return histogram;
-
+        
     }
 
     public  static double[] percentage_per_hand_catergory_stochastic(int hand_size, int random_samples){
-        double [] histogram = new double[10];
+    	
+    	double [] histogram = new double[10];
+    	
+    	int totalHands = 0;
+    	
+    	for(int index=0; index<random_samples; index++) {
+    		
+			Hand hand = new Hand(hand_size);
+			histogram[hand.rank.ordinal()]++;
+			totalHands++;
+			System.out.println(index);
+    		
+    	}
+    	
+    	for(int index=0; index<histogram.length; index++) {
+    		histogram[index] = histogram[index]/totalHands;
+    		histogram[index] = histogram[index] * 100;
+    	}
 
-        for(int count=0; count<random_samples; count++){
-            Hand hand = new Hand(5);
-
-            histogram[hand.rank.getRankValue()]++;
-        }
         return histogram;
     }
 }
