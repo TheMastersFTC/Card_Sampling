b341cb30ef5ca3afdabf5f434fb4746608405d7f
ky-grr <kyl.flu@gmail.com>
2017-03-23 01:49:10 -0600

    FINAL - REMOVED HASH SET FROM HERE, PUT IN HAND WHERE IT SHOULD BE

--- src/cards/Deck.java
+++ src/cards/Deck.java
@@ -1,13 +1,11 @@
 package cards;
 
-import java.util.HashSet;
-
 /**
  * Created by chloe on 3/14/2017.
  */
 public class Deck {
-    static HashSet<Integer> set = new HashSet<>();
-    static final Card [] deck = new Card[52];
+
+	static final Card [] deck = new Card[52];
 
     static {
         for (int count=0; count < 52; count++) {
@@ -34,20 +32,6 @@ public class Deck {
         return (num+2)-(13*suit.getMultiplier());
     }
  
-    /**
-     * checks to see if the position was already added in the set
-     * returns true if the position was added.
-     *
-     * @param position
-     * @return
-     */
-    public static boolean noDublicates(int position){
 
-        if(set.contains(position)==false){
-            set.add(position);
-            return true;
-        }
-        return false;
-    }
 
 }
