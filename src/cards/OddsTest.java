cf7271a3ef7fd329dd59a09f76fa5f7be0b50492
TheMastersFTC <chloejosien@gmail.com>
2017-03-23 09:37:09 -0600

    added comments

--- src/cards/OddsTest.java
+++ src/cards/OddsTest.java
@@ -4,31 +4,32 @@ import java.util.ArrayList;
 import static junit.framework.TestCase.assertEquals;
 
 /**
- * Created by chloe on 3/20/2017.
+ * Created by chloe Josien and Kylee fluckiger
+ * tests the odds class 
  */
 public class OddsTest {
 
 //    @Test
 //    public void percentageExhaustiveTest() {
-//        
+//
 //    	double [] histogram = Odds.percentage_per_hand_category_exhaustive(5);
 //    	for(double bin : histogram) {
 //    		System.out.println(bin);
 //    	}
 //    }
-    
+
     @Test
     public void percentageExhaustiveTestSeven(){
-        
-    	double [] histogram = Odds.percentage_per_hand_category_exhaustive(7);
-    	for(double bin : histogram) {
-    		System.out.println(bin);
-    	}
+
+        double [] histogram = Odds.percentage_per_hand_category_exhaustive(7);
+        for(double bin : histogram) {
+            System.out.println(bin);
+        }
     }
-    
+
 //    @Test
 //    public void hierarchy() {
-//    	
+//
 //        Card one = new Card(Constant.Suit.CLUB,2);
 //        Card two = new Card(Constant.Suit.CLUB,3);
 //        Card three = new Card(Constant.Suit.CLUB,10);
@@ -38,14 +39,14 @@ public class OddsTest {
 //        Card seven = new Card(Constant.Suit.DIAMOND, 14);
 //
 //        Hand hand = new Hand(one, two, three, four, five, six, seven);
-//        
+//
 //        System.out.println(hand.rank);
-//          	
+//
 //    }
-	
+
 //  @Test
 //  public void hierarchy() {
-//  	
+//
 //      Card one = new Card(Constant.Suit.DIAMOND,1);
 //      Card two = new Card(Constant.Suit.HEART,1);
 //      Card three = new Card(Constant.Suit.HEART,2);
@@ -55,19 +56,19 @@ public class OddsTest {
 //      Card seven = new Card(Constant.Suit.HEART, 6);
 //
 //      Hand hand = new Hand(one, two, three, four, five, six, seven);
-//      
+//
 //      System.out.println(hand.rank);
 //      System.out.println(hand.longestStraight());
-//        	
-  //}
-    
+//
+    //}
+
 //    @Test
 //    public void stochasticFive(){
-//        
+//
 //    	double [] histogram = Odds.percentage_per_hand_catergory_stochastic(7, 1_000_000);
 //    	for(double bin : histogram) {
 //    		System.out.println(bin);
 //    	}
 //    }
-    
+
 }
