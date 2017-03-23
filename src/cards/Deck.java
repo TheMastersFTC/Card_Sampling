4eba0e1a77ccefb91ee68ed9d729c0e7aca7cc7b
TheMastersFTC <chloejosien@gmail.com>
2017-03-23 09:57:35 -0600

    added comments/ formatted

--- src/cards/Deck.java
+++ src/cards/Deck.java
@@ -1,37 +1,41 @@
 package cards;
 
 /**
- * Created by chloe on 3/14/2017.
+ * Created by chloe Josien and kylee Fluckiger
+ * creates the deck of cards
  */
 public class Deck {
 
-	static final Card [] deck = new Card[52];
+    static final Card[] deck = new Card[52];
 
     static {
-        for (int count=0; count < 52; count++) {
+        for (int count = 0; count < 52; count++) {
             Constant.Suit suit;
-            if(count<13){
+            if (count < 13) {
                 suit = Constant.Suit.CLUB;
-                deck[count]= new Card(suit,value(suit,count));
-            }
-            else if(count<26){
+                deck[count] = new Card(suit, value(suit, count));
+            } else if (count < 26) {
                 suit = Constant.Suit.DIAMOND;
-                deck[count]= new Card(suit,value(suit,count));
-            }
-            else if(count<39){
+                deck[count] = new Card(suit, value(suit, count));
+            } else if (count < 39) {
                 suit = Constant.Suit.HEART;
-                deck[count]= new Card(suit,value(suit,count));
-            }
-            else {
+                deck[count] = new Card(suit, value(suit, count));
+            } else {
                 suit = Constant.Suit.SPADE;
-                deck[count]= new Card(suit,value(suit,count));
+                deck[count] = new Card(suit, value(suit, count));
             }
         }
     }
-    public static int value(Constant.Suit suit, int num){
-        return (num+2)-(13*suit.getMultiplier());
-    }
- 
 
+    /**
+     * returns the value on the card
+     *
+     * @param suit
+     * @param num
+     * @return
+     */
+    public static int value(Constant.Suit suit, int num) {
+        return (num + 2) - (13 * suit.getMultiplier());
+    }
 
 }
