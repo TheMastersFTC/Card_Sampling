f1d81b620d5f6a68d68890b7fd62cd9462e3338d
ky-grr <kyl.flu@gmail.com>
2017-03-22 23:59:42 -0600

    test for Stochastic class

--- src/cards/OddsTest.java
+++ src/cards/OddsTest.java
@@ -9,14 +9,14 @@ import static junit.framework.TestCase.assertEquals;
 public class OddsTest {
 
 //    @Test
-//    public void percentageExhaustiveTest(){
+//    public void percentageExhaustiveTest() {
 //        
 //    	double [] histogram = Odds.percentage_per_hand_category_exhaustive(5);
 //    	for(double bin : histogram) {
 //    		System.out.println(bin);
 //    	}
 //    }
-//    
+    
 //    @Test
 //    public void percentageExhaustiveTestSeven(){
 //        
@@ -26,21 +26,48 @@ public class OddsTest {
 //    	}
 //    }
     
+//    @Test
+//    public void hierarchy() {
+//    	
+//        Card one = new Card(Constant.Suit.CLUB,2);
+//        Card two = new Card(Constant.Suit.CLUB,3);
+//        Card three = new Card(Constant.Suit.CLUB,10);
+//        Card four = new Card(Constant.Suit.CLUB,11);
+//        Card five = new Card(Constant.Suit.CLUB,12);
+//        Card six = new Card(Constant.Suit.CLUB,13);
+//        Card seven = new Card(Constant.Suit.DIAMOND, 14);
+//
+//        Hand hand = new Hand(one, two, three, four, five, six, seven);
+//        
+//        System.out.println(hand.rank);
+//          	
+//    }
+	
+//  @Test
+//  public void hierarchy() {
+//  	
+//      Card one = new Card(Constant.Suit.DIAMOND,1);
+//      Card two = new Card(Constant.Suit.HEART,1);
+//      Card three = new Card(Constant.Suit.HEART,2);
+//      Card four = new Card(Constant.Suit.HEART,3);
+//      Card five = new Card(Constant.Suit.HEART,4);
+//      Card six = new Card(Constant.Suit.HEART,5);
+//      Card seven = new Card(Constant.Suit.HEART, 6);
+//
+//      Hand hand = new Hand(one, two, three, four, five, six, seven);
+//      
+//      System.out.println(hand.rank);
+//      System.out.println(hand.longestStraight());
+//        	
+  //}
+    
     @Test
-    public void hierarchy() {
-    	
-        Card one = new Card(Constant.Suit.CLUB,2);
-        Card two = new Card(Constant.Suit.CLUB,3);
-        Card three = new Card(Constant.Suit.CLUB,10);
-        Card four = new Card(Constant.Suit.CLUB,11);
-        Card five = new Card(Constant.Suit.CLUB,12);
-        Card six = new Card(Constant.Suit.CLUB,13);
-        Card seven = new Card(Constant.Suit.DIAMOND, 14);
-
-        Hand hand = new Hand(one, two, three, four, five, six, seven);
+    public void stochasticFive(){
         
-        System.out.println(hand.rank);
-          	
+    	double [] histogram = Odds.percentage_per_hand_catergory_stochastic(5, 120);
+    	for(double bin : histogram) {
+    		System.out.println(bin);
+    	}
     }
     
 }
