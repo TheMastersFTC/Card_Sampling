d115c55dbf5e0f0160e3692790075c53412bfc5c
ky-grr <kyl.flu@gmail.com>
2017-03-22 20:35:57 -0600

    Add files via upload

--- src/cards/OddsTest.java
+++ src/cards/OddsTest.java
@@ -8,13 +8,39 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class OddsTest {
 
+//    @Test
+//    public void percentageExhaustiveTest(){
+//        
+//    	double [] histogram = Odds.percentage_per_hand_category_exhaustive(5);
+//    	for(double bin : histogram) {
+//    		System.out.println(bin);
+//    	}
+//    }
+//    
+//    @Test
+//    public void percentageExhaustiveTestSeven(){
+//        
+//    	double [] histogram = Odds.percentage_per_hand_category_exhaustive(7);
+//    	for(double bin : histogram) {
+//    		System.out.println(bin);
+//    	}
+//    }
+    
     @Test
-    public void percentageExhaustiveTest(){
+    public void hierarchy() {
+    	
+        Card one = new Card(Constant.Suit.CLUB,2);
+        Card two = new Card(Constant.Suit.CLUB,3);
+        Card three = new Card(Constant.Suit.CLUB,10);
+        Card four = new Card(Constant.Suit.CLUB,11);
+        Card five = new Card(Constant.Suit.CLUB,12);
+        Card six = new Card(Constant.Suit.CLUB,13);
+        Card seven = new Card(Constant.Suit.DIAMOND, 14);
+
+        Hand hand = new Hand(one, two, three, four, five, six, seven);
         
-    	double [] histogram = Odds.percentage_per_hand_category_exhaustive(5);
-    	for(double bin : histogram) {
-    		System.out.println(bin);
-    	}
+        System.out.println(hand.rank);
+          	
     }
     
 }
